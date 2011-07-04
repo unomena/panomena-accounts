4de41c6bfa8f40bfd1ce7b00d6f8443dd21bef6c
Jurie-Jan Botha <juriejanbotha@gmail.com>
2011-07-04 11:50:52 +0200

    Added general dependency

--- setup.py
+++ setup.py
@@ -10,10 +10,12 @@ setup(
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     dependency_links = [
+        'http://github.com/unomena/panomena-general/tarball/master#egg=panomena-general',
         'http://github.com/unomena/panomena-mobile/tarball/master#egg=panomena-mobile',
     ],
     install_requires = [
         'Django==1.2.5',
+        'panomena-general==0.0.1',
         'panomena-mobile==0.0.1',
     ],
 )
