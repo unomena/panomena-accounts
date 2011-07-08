0fe14313d6c519107de848917bb08bb897ffd19b
Jurie-Jan Botha <juriejanbotha@gmail.com>
2011-07-08 10:03:41 +0200

    Removed some imports

--- src/panomena_accounts/views.py
+++ src/panomena_accounts/views.py
@@ -5,8 +5,8 @@ from django.contrib.auth.views import logout as auth_logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
-from panomena_general.utils import class_from_string, json_redirect, \
-    SettingsFetcher, is_ajax_request, ajax_redirect
+from panomena_general.utils import class_from_string, SettingsFetcher, \
+    ajax_redirect
 
 from panomena_accounts.forms import AvatarForm, ForgotSMSForm
 from panomena_accounts.utils import get_profile_model
