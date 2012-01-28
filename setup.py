0a20895e63526cdd2edcc587a2b0bea218f9eaa1
Euan Jonker <euan@unomena.com>
2012-01-28 09:03:52 +0200

    Django=>1.2.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='panomena-accounts',
     description='Panomena Accounts',
-    version='0.0.2',
+    version='0.0.3',
     author='',
     license='Proprietory',
     url='http://www.unomena.com/',
@@ -14,7 +14,7 @@ setup(
         'http://github.com/unomena/panomena-mobile/tarball/master#egg=panomena-mobile',
     ],
     install_requires = [
-        'Django==1.2.5',
+        'Django=<1.2.5',
         'panomena-general==0.0.1',
         'panomena-mobile==0.0.1',
     ],
